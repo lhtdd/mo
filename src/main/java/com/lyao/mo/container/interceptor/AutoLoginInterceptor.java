4bf2cf16ecb3f50a29f047043dd21e020bcb8a4a
lhtdd <773121935@qq.com>
2017-10-12 00:28:44 +0800

    10.12

--- src/main/java/com/lyao/mo/container/interceptor/AutoLoginInterceptor.java
+++ src/main/java/com/lyao/mo/container/interceptor/AutoLoginInterceptor.java
@@ -1,5 +1,8 @@
 package com.lyao.mo.container.interceptor;
 
+import java.net.URLDecoder;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,20 +12,34 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.lyao.mo.common.utils.Constant;
 
+public class AutoLoginInterceptor extends HandlerInterceptorAdapter {
 
-public class AutoLoginInterceptor extends HandlerInterceptorAdapter{
-	
 	private final Logger log = Logger.getLogger(AutoLoginInterceptor.class);
+
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		//1.只有未登录的用户才能自动登陆  
-        if(request.getSession().getAttribute(Constant.CURRENT_USER) == null){  
-              
-        }  
+		// 1.只有未登录的用户才能自动登陆
+		if (request.getSession().getAttribute(Constant.CURRENT_USER) == null) {
+			String cookie_username = null;
+			String cookie_password = null;
+			// 获得cookie
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					// 获得名字是cookie_username和cookie_password
+					if (Constant.COOKIE_USERNAME.equals(cookie.getName())) {
+						cookie_username = URLDecoder.decode(cookie.getValue(),"UTF-8");
+					}
+					if (Constant.COOKIE_PASSWORD.equals(cookie.getName())) {
+						cookie_password = URLDecoder.decode(cookie.getValue(),"UTF-8");
+					}
+				}
+			}
+		}
 		return true;
 	}
-	
+
 	@Override
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
@@ -30,7 +47,7 @@ public class AutoLoginInterceptor extends HandlerInterceptorAdapter{
 		// TODO Auto-generated method stub
 		super.postHandle(request, response, handler, modelAndView);
 	}
-	
+
 	@Override
 	public void afterCompletion(HttpServletRequest request,
 			HttpServletResponse response, Object handler, Exception ex)
