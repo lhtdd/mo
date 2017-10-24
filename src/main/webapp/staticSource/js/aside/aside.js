b88356a7e389c86153d923058a646f0a81027d53
lhtdd <773121935@qq.com>
2017-10-24 23:43:11 +0800

    10.24

--- src/main/webapp/staticSource/js/aside/aside.js
+++ src/main/webapp/staticSource/js/aside/aside.js
@@ -125,5 +125,56 @@ $(function() {
 			});
 		}
 	})
-	
+	//刷新用户下的某个收藏夹
+	function refreshURL(cusid,navid){
+		$.ajax({
+		    url:'',
+		    type:'GET',
+		    async:true,
+		    timeout:5000,
+		    dataType:'json',
+		    success:function(data){
+		        if (data.flag == 'yes'){
+		        	$.each(data.navFolder,function(key,values){ 
+			        		var str = "<li class='aside-bar-item'>";
+					        		str += "<input type='hidden' name='cur_folder_id' value="+values.folderID+">";
+									str += "<i class='iconfont "+values.folderIcon+"'></i>";
+									str += "<span>"+values.folderName+"</span>";
+									if (values.URLS != null){
+										str += "<div class='aside-bar-content scrollbar-aside'>";
+											str += "<div class='scrollbar-wrap'>";
+												str += "<ul>";
+												$.each(values.URLS,function(index,url){
+													str += "<li class='url-item'>";
+														str += "<div class='url-content' style='background:url(/mo/staticSource/image/"+url.urlimage+") no-repeat left center;'>";
+															str += "<a class='url-info' href='"+url.url+"' target='_blank'><em title='"+url.urlname+"'>"+url.urlname+"</em></a>";
+															str += "<input type='hidden' name='cur_url_id' value="+url.id+">";
+															str += "<div class='url-edit'>";
+																	str += "<i class='iconfont icon-xiugai fs12'></i>";
+																	str += "<div class='url-edit-tip'>";
+																		str += "<ul>";
+																			str += "<li>编辑</li>";
+																			str += "<li>删除</li>";
+																		str += "</ul>";
+																	str += "</div>";
+															str += "</div>";
+														str += "</div>";
+													str += "</li>";
+												})
+												str += "</ul>";
+											str += "</div>";
+										str += "</div>";
+									}
+									
+								str += "</li>";
+						$(".aside-bar-other").before(str);
+		        	})
+		        	//重新绘制滚动条
+		        	$('.scrollbar-aside').niceScroll(".scrollbar-wrap", {cursorcolor : "#FF6600"}).resize();
+		        }else{
+		        	layer.msg(data.errorMsg);
+		        }
+		    }
+		});
+	}
 });
